37612fb9ca9fac5fdd5ea98a814a4df018adc62c
iwy2th <huyloind03@gmail.com>
2023-06-02 11:02:18 +0700

    drag feature combined with a func scale up feature

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -11,51 +11,69 @@ struct ContentView: View {
   // MARK: - PROPERTY
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
   // MARK: - FUNCTION
-
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
 
   // MARK: - CONTENT
-    var body: some View {
-      NavigationView {
-        ZStack{
-          // MARK: - PAGE IMAGE
-          Image("magazine-front-cover")
-            .resizable()
-            .scaledToFit()
-           // .aspectRatio(contentMode: .fit)
-            .cornerRadius(10)
-            .padding()
-            .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-            .opacity(isAnimating ? 1 : 0)
-            .scaleEffect(imageScale)
-          // MARK: - TAP GESTURE
-            .onTapGesture(count: 2) {
-              if imageScale == 1 {
-                withAnimation(.spring()) {
-                  imageScale = 5
-                }
-              } else {
-                withAnimation {
-                  imageScale = 1
-                }
+  var body: some View {
+    NavigationView {
+      ZStack{
+        // MARK: - PAGE IMAGE
+        Image("magazine-front-cover")
+          .resizable()
+          .scaledToFit()
+        // .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
+          .scaleEffect(imageScale)
+        // MARK: - TAP GESTURE
+          .onTapGesture(count: 2) {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 3
               }
+            } else {
+             resetImageState()
             }
-
-        } //: ZSTACK
-        .navigationTitle("Pinch & Zoom")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-          withAnimation(.linear(duration: 1)) {
-            isAnimating = true
           }
+        // MARK: - DRAG GESTURE
+          .gesture(DragGesture()
+            .onChanged({ gesture in
+              withAnimation(.linear(duration: 1)) {
+                imageOffset = gesture.translation
+              }
+            })
+              .onEnded({ _ in
+                if imageScale <= 1 {
+                 resetImageState()
+                }
+              }))
+
+
+      } //: ZSTACK
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
         }
-      } //: NAVIGATION
-      .navigationViewStyle(.stack)
-    }
+      }
+    } //: NAVIGATION
+    .navigationViewStyle(.stack)
+  }
 }
 // MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
