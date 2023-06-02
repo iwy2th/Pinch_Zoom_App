979520d0ab4a52c335899c9a64c4f7510928d794
iwy2th <huyloind03@gmail.com>
2023-06-02 10:03:39 +0700

    start

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Iwy2th on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
